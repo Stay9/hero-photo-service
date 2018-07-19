0bc3851ff18637dc27d62f1d69995d7222836c08
JP Vertil <vertil.jp@gmail.com>
2018-07-18 19:50:41 -0700

    implement script to populate database with self generated data

--- server/index.js
+++ server/index.js
@@ -1,18 +1,17 @@
 const express = require('express');
+
 const app = express();
 
-// making a middleware to tarck all incoming requests 
+// making a middleware to tarck all incoming requests
 app.use((req, res, next) => {
-	console.log('Request method: ', req.method);
-	next();
-})
-
+  console.log('Request method: ', req.method);
+  next();
+});
 
 
 // spotholder for middleware of static assets
 
 
-
 app.listen(3000, () => {
-	console.log('Hero Photo server is listening on port 3000');
-})
+  console.log('Hero Photo server is listening on port 3000');
+});
