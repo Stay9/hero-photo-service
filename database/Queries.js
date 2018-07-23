d6b5cb6e908f76882f0798ab91ac788233389e6c
JP Vertil <vertil.jp@gmail.com>
2018-07-23 11:55:13 -0700

    created function to query all data from the listing_photos tables

--- database/Queries.js
+++ database/Queries.js
@@ -1,2 +1,22 @@
 // model make queries to the hero database
 const connection = require('./index.js');
+
+const getListingPhotos = (listingId, whenGotten) => {
+  const theQuery = `SELECT * FROM listing_photos WHERE photo_listing_id = ${listingId}`;
+  connection.query(theQuery, (err, res) => {
+    if (err) {
+      console.log('Database side error in retrieving data from the listing_photos table ', err);
+      whenGotten(err);
+    } else {
+      console.log('Database side success in retriving data from the listing_photos table ', res);
+      whenGotten(res);
+    }
+  });
+};
+
+module.exports = {
+  getListingPhotos,
+};
+
+
+// getListingPhotos(2, (err, res) =>{}); // testing
