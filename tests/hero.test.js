cd4b518d69d0d679aaa6f3e86f9a09d796cb50bf
JP Vertil <vertil.jp@gmail.com>
2018-07-29 00:58:06 -0700

    set environment for testing with enzyme

---
+++ tests/hero.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Hero from '../client/src/components/Hero.jsx';
+import { shallow, mount, render } from 'enzyme';
+
+const wrapper = mount(<Hero/>);
+
+describe('Hero tests', () => {
+
+});
