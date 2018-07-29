e260eda6eacea67c051b7a65449aa10d862ca186
JP Vertil <vertil.jp@gmail.com>
2018-07-29 02:23:02 -0700

    setup to have enzyme work with css modules

--- tests/hero.test.js
+++ tests/hero.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Hero from '../client/src/components/Hero.jsx';
 import { shallow, mount, render } from 'enzyme';
-
-const wrapper = mount(<Hero/>);
+import stylesClass from '../client/src/components/hero.css';
+// Step 1: this next line is important since the finders expect a '.' at the string start
+const wrapper = shallow(<Hero/>);
 
 describe('Hero tests', () => {
+ test('Paws should render correct # of full paw icons', () => {
+   expect(wrapper.find('.hero')).to.have.lengthOf(1);
+ });
 
 });
+
