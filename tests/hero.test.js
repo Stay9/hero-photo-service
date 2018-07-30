4e6f21d1b2e2003532123b6e18e9376faf4a6e03
JP Vertil <vertil.jp@gmail.com>
2018-07-29 17:39:51 -0700

    completed several tests for the hero component

--- tests/hero.test.js
+++ tests/hero.test.js
@@ -2,13 +2,63 @@ import React from 'react';
 import Hero from '../client/src/components/Hero.jsx';
 import { shallow, mount, render } from 'enzyme';
 import stylesClass from '../client/src/components/hero.css';
+import sinon from 'sinon';
 // Step 1: this next line is important since the finders expect a '.' at the string start
-const wrapper = shallow(<Hero/>);
+
 
 describe('Hero tests', () => {
- test('Paws should render correct # of full paw icons', () => {
-   expect(wrapper.find('.hero')).to.have.lengthOf(1);
+ test('No modal should show, thus being false', () => {
+ 	const wrapper = shallow(<Hero/>);
+	const showGalleryState = wrapper.state().showGalleryModal;
+	const showShareState = wrapper.state().showShareModal;
+	const showSaveState = wrapper.state().showSaveModal;
+    expect(showGalleryState).toEqual(false);
+    expect(showShareState).toEqual(false);
+    expect(showSaveState).toEqual(false);
  });
 
+ test('Should open the Gallery Model when the gallery button is clicked', () => {
+ 	const wrapper = shallow(<Hero/>);
+ 	const galButton = wrapper.find('.hero-view-photos-button');
+ 	galButton.simulate('click');
+ 	const showGalleryState = wrapper.state().showGalleryModal;
+ 	expect(showGalleryState).toEqual(true);
+ });
+
+ test('Should open the Sahre Model when the share button is clicked', () => {
+ 	const wrapper = shallow(<Hero/>);
+ 	const shareButton = wrapper.find('.hero-share-button');
+ 	shareButton.simulate('click');
+ 	const showShareState = wrapper.state().showShareModal;
+ 	expect(showShareState).toEqual(true);
+ });
+
+ test('Should open the Save Model when the save button is clicked', () => {
+ 	const wrapper = shallow(<Hero/>);
+ 	const saveButton = wrapper.find('.hero-save-button');
+ 	saveButton.simulate('click');
+	const showSaveState = wrapper.state().showSaveModal;
+ 	expect(showSaveState).toEqual(true);
+ });
+
+/* test('Should invoke handShowGallery', () => {
+ 	const onButtonClick = sinon.spy();
+ 	const wrapper = shallow(<Hero/>);
+ 	wrapper.find('.hero-view-photos-button').simulate('click');
+ 	expect(onButtonClick.calledOnce).to.equal(true);
+ })*/
+
+ test('Should call didMount', () => {
+ 	const didMount = sinon.spy();
+ 	
+ })
+
+
 });
+
+
+
+
+
+
 
