cdf500764417a20b510c3b5b8d4e4130666456da
Chloe Lin <chloewjlin@gmail.com>
2018-08-17 19:13:11 -0700

    Connect to PostgreSQL using node-postgres

--- database/index.js
+++ database/index.js
@@ -1,20 +1,29 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'hero',
+
+// });
+
+// connection.connect((err) => {
+  // if (err) {
+  //   console.log('Error connecting to mysql ', err);
+  // } else {
+  //   console.log('Successful connection to mysql');
+  // }
+// });
+
+// connect to psql
+const { Pool } = require('pg');
+
+const connection = new Pool({
+  user: '',
   host: 'localhost',
-  user: 'root',
+  database: 'sdc',
   password: '',
-  database: 'hero',
-
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.log('Error connecting to mysql ', err);
-  } else {
-    console.log('Successful connection to mysql');
-  }
-});
-
+})
 
 module.exports = connection;
