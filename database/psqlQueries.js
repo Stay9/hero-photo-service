d141e8fc4495ffe23b8d91f1e922426079888a4f
Chloe Lin <chloewjlin@gmail.com>
2018-08-17 19:16:24 -0700

    Design parameterized query to avoid sql injection vulnerabilities

---
+++ database/psqlQueries.js
@@ -0,0 +1,28 @@
+const connection = require('./index.js');
+
+// Get all the photos and listing details associated with one listing 
+const getListingPhotos = (listingId, whenGotten) => {
+  const Query = `SELECT * FROM listings INNER JOIN photos ON listing_id = listings.id WHERE listing_id = $1;`;
+  connection.query(Query, [listingId], (err, res) => {
+    if (err) {
+      console.error('Database side error in retrieving data from the listings table ', err);
+      whenGotten(err);
+    } else {
+      console.log('Database side success in retriving data from the listings table ', res);
+      whenGotten(null, res);
+    }
+  });
+}; 
+
+// test query 
+// getListingPhotos(1, (err, res) => {
+//   if (err) {
+//     console.log('Fail to get listing');
+//   } else {
+//     console.log('Successfully get listing');
+//   }
+// });
+
+module.exports = {
+  getListingPhotos,
+}
