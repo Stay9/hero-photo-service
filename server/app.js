c0eefae2cd1c99cee77f4ac8e2f372e0e6b9e667
Chloe Lin <chloewjlin@gmail.com>
2018-08-21 16:08:41 -0700

    Use csv file to loadtest and reduce response time to 2ms
    Issue: Using csv reduced response time from 200ms to 2ms, but it
    also significantly reduced throughput from 24k to less than 8k.
    
    Nest step: use an array of 100 listing ids in the last 10% of 10M
    data to stress-test server

--- server/app.js
+++ server/app.js
@@ -3,22 +3,18 @@ require('newrelic');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-// const Queries = require('../database/Queries.js'); // for mySQL
-const Queries = require('../database/psqlQueries.js'); // for PostgreSQL
+const Queries = require('../database/psqlQueries.js'); 
 const compression = require('compression');
 const redis = require("redis");
 const client = redis.createClient();
 
 const app = express();
 
-// making a middleware to tarck all incoming requests
 app.use((req, res, next) => {
-  // console.log('Request method: ', req.method);
-  compression(); // GZIP all assets
+  compression(); 
   next();
 });
 
-// access the static files
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use(bodyParser.json());
@@ -31,9 +27,8 @@ const getPhotos = (req, res) => {
     } else {
       Queries.getListingPhotos(listingId, (err, results) => {
         if (err) {
-          console.log('Server side error in query to get data from the listings_data table', err);
+          res.sendStatus(500).send(err);
         } else {
-          // console.log('Server side success in query to get data from the listings_data table');
           res.json(results);
         }
       });
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
-
 const app = require('./app.js');
 
 app.listen(3000, (err) => {
